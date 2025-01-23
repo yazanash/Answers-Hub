dce0cdfb9b9341511f0995d51acbd0675f2b5354
yazanash <yazan.ash.doonaas@gmail.com>
2025-01-23 13:18:36 +0300

    Add group services

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('/categories', App\Http\Controllers\CategoryController::class);
+Route::resource('/groups', App\Http\Controllers\GroupController::class);
 
 Route::get('/posts', [App\Http\Controllers\PostController::class, 'index'])->name('posts');
 Route::get('/posts/create', [App\Http\Controllers\PostController::class, 'create'])->name('posts.create');
@@ -25,16 +26,3 @@ Route::post('/questions/store', [App\Http\Controllers\QuestionController::class,
 Route::get('/questions/edit/{$question}', [App\Http\Controllers\QuestionController::class, 'edit'])->name('questions.edit');
 Route::post('/questions/update/{$question}', [App\Http\Controllers\QuestionController::class, 'update'])->name('questions.update');
 
-// Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'index'])->name('categories');
-// Route::get('/categories/create', [App\Http\Controllers\CategoryController::class, 'create'])->name('categories.create');
-// Route::post('/categories/store', [App\Http\Controllers\CategoryController::class, 'store'])->name('categories.store');
-// Route::get('/categories/edit/{$category}', [App\Http\Controllers\CategoryController::class, 'edit'])->name('categories.edit');
-// Route::post('/categories/update/{$category}', [App\Http\Controllers\CategoryController::class, 'update'])->name('categories.update');
-// Route::post('/categories/{$category}', [App\Http\Controllers\CategoryController::class, 'show'])->name('categories.show');
-// Route::post('/categories/destroy/{$category}', [App\Http\Controllers\CategoryController::class, 'destroy'])->name('categories.destroy');
-
-Route::get('/groups', [App\Http\Controllers\GroupController::class, 'index'])->name('groups');
-Route::get('/groups/create', [App\Http\Controllers\GroupController::class, 'create'])->name('groups.create');
-Route::post('/groups/store', [App\Http\Controllers\GroupController::class, 'store'])->name('groups.store');
-Route::get('/groups/edit/{$group}', [App\Http\Controllers\GroupController::class, 'edit'])->name('groups.edit');
-Route::post('/groups/update/{$group}', [App\Http\Controllers\GroupController::class, 'update'])->name('groups.update');
